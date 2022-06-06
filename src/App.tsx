66786a1c89883d44308fb783ff281e96ddde138f
Rory Maguire <rorymaguire00@gmail.com>
2022-06-06 11:56:51 +0100

    display container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 //styles
+import Display from "./Components/Display";
 import styles from "./Styles/App.module.css";
 
 export function App() {
-	return <div className={styles.container}></div>;
+	return (
+		<div className={styles.container}>
+			<Display />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/Display.tsx
@@ -0,0 +1,8 @@
+//styles
+import styles from "../Styles/Display.module.css";
+
+const Display = () => {
+	return <div className={styles.display}></div>;
+};
+
+export default Display;
