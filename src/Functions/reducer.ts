0c345f784acaab6c3bc1f20be470a511b65956d3
Rory Maguire <rorymaguire00@gmail.com>
2022-06-06 15:10:14 +0100

    ACTIONS.ADD_NUMBER

--- src/Functions/reducer.ts
+++ src/Functions/reducer.ts
@@ -1,8 +1,18 @@
-import { ICalculatorActions, IDisplayState } from "../Structures";
+import { ICalculatorActions } from "../Structures";
+import { IDisplayState } from "../Structures";
+import { ACTIONS } from "../Structures";
 
 export const reducer = (
 	state: IDisplayState,
 	action: ICalculatorActions
 ): IDisplayState => {
+	switch (action.type) {
+		case ACTIONS.ADD_NUMBER:
+			//spread state and overwrite current value by appending value of pressed btn
+			return {
+				...state,
+				currentValue: `${state.currentValue}${action.payload}`,
+			};
+	}
 	return { currentValue: "", previousValue: "", operation: "" };
 };
