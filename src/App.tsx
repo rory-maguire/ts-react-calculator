7ef63bf964821fd56ad4743349528c804ee8e352
Rory Maguire <rorymaguire00@gmail.com>
2022-06-06 12:03:44 +0100

    calculator body container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 //styles
-import Display from "./Components/Display";
 import styles from "./Styles/App.module.css";
+//components
+import Display from "./Components/Display";
+import Body from "./Components/Body";
 
 export function App() {
 	return (
 		<div className={styles.container}>
 			<Display />
+			<Body />
 		</div>
 	);
 }
---
+++ src/Components/Body.tsx
@@ -0,0 +1,8 @@
+//styles
+import styles from "../Styles/Body.module.css";
+
+const Body = () => {
+	return <div className={styles.body}></div>;
+};
+
+export default Body;
