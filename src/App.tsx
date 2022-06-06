294eee292b2c1baa87ecaa0fe52d61b15d259889
Rory Maguire <rorymaguire00@gmail.com>
2022-06-06 11:45:40 +0100

    calculator container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-function App() {
-	return <div className="App"></div>;
+//styles
+import styles from "./Styles/App.module.css";
+
+export function App() {
+	return <div className={styles.container}></div>;
 }
 
 export default App;
