6251405cc265f3f71a885d17c9e4628435d762bd
Rory Maguire <rorymaguire00@gmail.com>
2022-06-06 13:42:52 +0100

    representing keypad buttons and operator buttons as arrays to map through

---
+++ src/Structures/index.ts
@@ -0,0 +1,15 @@
+export const keypad: string[] = [
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"del",
+	"0",
+	".",
+];
+export const operators: string[] = ["+", "-", "x", "÷"];
